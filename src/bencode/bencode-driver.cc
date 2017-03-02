4d6f967cc25a282f594aa711b33cad64b4909386
Ashkan Kiaie-Sandjie <kiaies_a@epita.fr>
2017-03-02 22:49:57 +0100

    bencoding: add BEncodeDriver class

---
+++ src/bencode/bencode-driver.cc
@@ -0,0 +1,17 @@
+#include "bencode-driver.hh"
+
+namespace BEncode
+{
+  BEncodeDriver::BEncodeDriver() { };
+
+  BEncodeType BDecode(std::string bufferInput)
+  {
+    // FIXME
+  }
+
+  std::string BEncode(BEncodeType node)
+  {
+    // FIXME
+  }
+
+} // namespace BEncode
---
+++ src/bencode/bencode-driver.hh
@@ -0,0 +1,16 @@
+#include "bencode-type.hh"
+
+namespace BEncode
+{
+  class BEncodeDriver
+  {
+  public:
+    BEncodeDriver();
+    ~BEncodeDriver() = default;
+
+    BEncodeType BDecode(std::string bufferInput);
+    std::string BEncode(BEncodeType node);
+
+  };
+
+} // namespace BEncode
