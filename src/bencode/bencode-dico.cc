bbe62b9830fa930b6d98215dc46014aa156a6fdc
Ashkan Kiaie-Sandjie <kiaies_a@epita.fr>
2017-03-03 23:15:37 +0100

    bencoding: add encoding / decoding for dico

--- src/bencode/bencode-dico.cc
+++ src/bencode/bencode-dico.cc
@@ -2,14 +2,60 @@
 
 namespace BEncode
 {
-  BEncodeDictionnary::BEncodeDictionnary(const std::string& input)
-    : BEncodeType(input)
+  BEncodeDictionnary::BEncodeDictionnary()
   { }
 
-  std::string BEncodeDictionnary::BEncode()
+  BEncodeDictionnary::BEncodeDictionnary(std::map<BEncodeString_ptr,
+					 BEncodeType_ptr> dico)
+  : dico_(dico)
+  { }
+
+  std::string BEncodeDictionnary::bEncode()
   {
-    // FIXME
+    value_ = "d";
+
+    for (auto i : dico_)
+    {
+      value_.append(i.first->getEncodedValue());
+      value_.append(i.second->getEncodedValue());
+    }
+
+    value_.append("e");
+
     return value_;
+  }
+
+  void BEncodeDictionnary::print(std::ostream& str) const
+  {
+    str << "{ ";
+    int tmp = 0;
+    for (auto i : dico_)
+    {
+      if (tmp++)
+        str << ", ";
+      i.first->print(str);
+      str << " => ";
+      i.second->print(str);
+    }
+    str << " }";
+  }
+
+  void BEncodeDictionnary::addPair(BEncodeString_ptr key, BEncodeType_ptr data)
+  {
+    dico_.emplace(std::make_pair(key, data));
+  }
+
+  std::ostream& operator<<(std::ostream& str, const BEncodeDictionnary& obj)
+  {
+    obj.print(str);
+    return str;
+  }
+
+  std::ostream& operator<<(std::ostream& str,
+			   const std::shared_ptr<BEncodeDictionnary>& obj)
+  {
+    obj->print(str);
+    return str;
   }
 
 } // namespace BEncode
--- src/bencode/bencode-dico.hh
+++ src/bencode/bencode-dico.hh
@@ -2,21 +2,39 @@
 
 #include "bencode-type.hh"
 #include "bencode-string.hh"
+
 #include <map>
+#include <memory>
+#include <iostream>
 
 namespace BEncode
 {
   class BEncodeDictionnary : public BEncodeType
   {
   public:
+    using BEncodeString_ptr = std::shared_ptr<BEncodeString>;
+    using BEncodeType_ptr = std::shared_ptr<BEncodeType>;
 
-    BEncodeDictionnary(const std::string& input);
+  public:
+    BEncodeDictionnary();
+    BEncodeDictionnary(std::map<BEncodeString_ptr, BEncodeType_ptr> dico);
+
     virtual ~BEncodeDictionnary() = default;
-    virtual std::string BEncode() override;
+    virtual std::string bEncode() override;
+    virtual void print(std::ostream& str) const override;
+
+    void addPair(BEncodeString_ptr key, BEncodeType_ptr data);
+
+    friend std::ostream&
+    operator<<(std::ostream& str,
+	       const BEncodeDictionnary& obj);
+
+    friend std::ostream&
+    operator<<(std::ostream& str,
+	       const std::shared_ptr<BEncodeDictionnary>& obj);
 
   private:
-    std::map<BEncodeString, BEncodeType> dico_;
-    std::string value_;
+    std::map<BEncodeString_ptr, BEncodeType_ptr> dico_;
   };
 
 } // namespace BEncode
