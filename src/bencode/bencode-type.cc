0d77961277b3a649b969d4e012476ae8ab315ecc
Ashkan Kiaie-Sandjie <kiaies_a@epita.fr>
2017-03-03 23:12:09 +0100

    bencoding: add default encoding / decoding in base class

--- src/bencode/bencode-type.cc
+++ src/bencode/bencode-type.cc
@@ -2,14 +2,37 @@
 
 namespace BEncode
 {
-  BEncodeType::BEncodeType(const std::string& input)
-    : input_(input)
+
+  BEncodeType::BEncodeType()
   { }
 
-  std::string BEncodeType::BEncode()
+  std::string BEncodeType::bEncode()
   {
-    // Default behaviour no encoding done
-    return input_;
+    return "";
+  }
+
+  void BEncodeType::bDecode(std::string&)
+  { }
+
+  void BEncodeType::print(std::ostream&) const
+  { }
+
+  std::string BEncodeType::getEncodedValue()
+  {
+    return value_;
+  }
+
+  std::ostream& operator<<(std::ostream& str, const BEncodeType& obj)
+  {
+    obj.print(str);
+    return str;
+  }
+
+  std::ostream& operator<<(std::ostream& str,
+			   const std::shared_ptr<BEncodeType>& obj)
+  {
+    obj->print(str);
+    return str;
   }
 
 } // namespace BEncode
--- src/bencode/bencode-type.hh
+++ src/bencode/bencode-type.hh
@@ -1,19 +1,28 @@
 #pragma once
 
 #include <string>
+#include <memory>
 
 namespace BEncode
 {
   class BEncodeType
   {
   public:
+    BEncodeType();
 
-    BEncodeType(const std::string& input);
     virtual ~BEncodeType() = default;
-    virtual std::string BEncode();
+    virtual std::string bEncode();
+    virtual void bDecode(std::string& bufferInput);
+    virtual void print(std::ostream& str) const;
 
-  private:
-    std::string input_;
+    friend std::ostream& operator<<(std::ostream& str, const BEncodeType& obj);
+    friend std::ostream& operator<<(std::ostream& str,
+				    const std::shared_ptr<BEncodeType>& obj);
+
+    std::string getEncodedValue();
+
+  protected:
+    std::string value_;
   };
 
 } // namespace BEncode
