7445f7447c2d5196d2c2e6e81162eb2440f821e8
Ashkan Kiaie-Sandjie <kiaies_a@epita.fr>
2017-03-03 23:14:52 +0100

    bencoding: add encoding / decoding for string

--- src/bencode/bencode-string.cc
+++ src/bencode/bencode-string.cc
@@ -1,15 +1,52 @@
 #include "bencode-string.hh"
+#include <iostream>
+#include <string>
 
 namespace BEncode
 {
-  BEncodeString::BEncodeString(const std::string& input)
-    : BEncodeType(input)
+  BEncodeString::BEncodeString() { }
+  BEncodeString::BEncodeString(std::string decode)
+    : decode_(decode)
   { }
 
-  std::string BEncodeString::BEncode()
+  std::string BEncodeString::bEncode()
   {
-    // FIXME
+    value_ = std::to_string(decode_.size());
+    value_.append(":");
+    value_.append(decode_);
     return value_;
+  }
+
+  void BEncodeString::bDecode(std::string& bufferInput)
+  {
+    auto pos = bufferInput.find(":");
+    if (pos == std::string::npos)
+    {
+      std::cout << "Invalid String encoding" << std::endl;
+      return;
+    }
+
+    auto len = std::stoll(bufferInput.substr(0, pos));
+    decode_ = bufferInput.substr(pos + 1, len);
+    bufferInput.erase(0, pos + len + 1);
+  }
+
+  void BEncodeString::print(std::ostream& str) const
+  {
+    str << "\"" << decode_ << "\"";
+  }
+
+  std::ostream& operator<<(std::ostream& str, const BEncodeString& obj)
+  {
+    obj.print(str);
+    return str;
+  }
+
+  std::ostream& operator<<(std::ostream& str,
+			   const std::shared_ptr<BEncodeString>& obj)
+  {
+    obj->print(str);
+    return str;
   }
 
 } // namespace BEncode
--- src/bencode/bencode-string.hh
+++ src/bencode/bencode-string.hh
@@ -7,13 +7,20 @@ namespace BEncode
   class BEncodeString : public BEncodeType
   {
   public:
+    BEncodeString();
+    BEncodeString(std::string decode);
 
-    BEncodeString(const std::string& input);
     virtual ~BEncodeString() = default;
-    virtual std::string BEncode() override;
+    virtual std::string bEncode() override;
+    virtual void bDecode(std::string& bufferInput) override;
+    virtual void print(std::ostream& str) const override;
+
+    friend std::ostream& operator<<(std::ostream& str, const BEncodeString& obj);
+    friend std::ostream& operator<<(std::ostream& str,
+				    const std::shared_ptr<BEncodeString>& obj);
 
   private:
-    std::string value_;
+    std::string decode_;
   };
 
 } // namespace BEncode
