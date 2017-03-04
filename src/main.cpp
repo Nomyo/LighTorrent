0230b2305e7e18852d118e1369f9d262bc38e295
Ashkan Kiaie-Sandjie <kiaies_a@epita.fr>
2017-03-04 22:46:15 +0100

    CMakeLists: add source files

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "network/tracker-connector.hh"
 #include "bencode/bencode-driver.hh"
+#include "core/url-utils.hh"
 
 int main(void)
 {
@@ -11,9 +12,11 @@ int main(void)
 
   std::string filename("tests/secretFamilyRecipes.torrent");
 
-  //BEncode::BEncodeDriver driver;
+  BEncode::BEncodeDriver driver;
 
-  //auto node = driver.bDecodeFile(filename);
-  //std::cout << node << std::endl;
+  auto node = driver.bDecodeFile(filename);
+  std::cout << node << std::endl;
+
+  Core::URLUtils url(*static_cast<BEncode::BEncodeDictionnary*>(&*node));
   return 0;
 }
