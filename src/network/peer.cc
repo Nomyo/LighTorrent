5a3902d5ddf074245003df975728a70813b58061
Ashkan Kiaie-Sandjie <kiaies_a@epita.fr>
2017-03-09 19:23:29 +0100

    network: peer: add torrent to constructor and tryHandshake method

--- src/network/peer.cc
+++ src/network/peer.cc
@@ -4,22 +4,35 @@
 namespace Network
 {
 
-  Peer::Peer(const std::string& ip, in_port_t port)
-    : ip_(ip)
+  Peer::Peer(Torrent *torrent, const std::string& ip, in_port_t port)
+    : torrent_(torrent)
+    , ip_(ip)
     , port_(port)
+    , fd_(0)
   { }
 
-  std::string Peer::getIp()
+  void Peer::tryHandshake()
   {
-    return ip_;
+    struct handshake hshake;
+
+    std::string info_hash = torrent_->getInfoHash();
+    std::string peer_id = torrent_->getPeerId();
+    std::string protocol = "BitTorrent protocol";
+
+    bzero(&hshake, sizeof (hshake));
+    hshake.pstrlen = 19;
+
+    memcpy(hshake.pstr, protocol.c_str(), 19);
+    memcpy(hshake.info_hash, info_hash.c_str(), 20);
+    memcpy(hshake.peer_id, peer_id.c_str(), 20);
+
+    // FIXME: RETURN SOMETHING TO LET THE PD REMOVE PEER
+    if (write(fd_, &hshake, sizeof (hshake)) < 0)
+      std::cerr << "Could not write request to peer \n";
+
   }
 
-  in_port_t Peer::getPort()
-  {
-    return port_;
-  }
-
-  void Peer::dump()
+  void Peer::dump() const
   {
     Core::ColorModifier clgreen(Core::ColorCode::FG_LIGHT_BLUE);
     Core::ColorModifier clblue(Core::ColorCode::FG_BLUE);
@@ -27,6 +40,22 @@ namespace Network
 
     std::cout << clblue << ip_  << " " << cdef << " "
 	      << clgreen << port_ << cdef << std::endl;
+  }
+
+  // Getter
+  std::string Peer::getIp() const
+  {
+    return ip_;
+  }
+
+  in_port_t Peer::getPort() const
+  {
+    return port_;
+  }
+
+  int Peer::getFd() const
+  {
+    return fd_;
   }
 
 } // namespace Network
--- src/network/peer.hh
+++ src/network/peer.hh
@@ -11,20 +11,38 @@ namespace Network
   class Peer
   {
 
+    struct handshake
+    {
+      uint8_t pstrlen;
+      uint8_t pstr[19];
+      uint8_t reserved[8];
+      uint8_t info_hash[20];
+      uint8_t peer_id[20];
+    };
+
   public:
     // Ctor & Dtor
     Peer(const std::string& ip, in_port_t port);
     ~Peer() = default;
 
-    std::string getIp();
-    in_port_t getPort();
 
-    void dump();
+    // Action
+    void tryHandshake();
+    void onReceive();
+
+    void dump() const;
+
+    // Getter
+    std::string getIp() const;
+    in_port_t getPort() const;
+    int getFd() const;
 
   private:
+    Torrent torrent_;
     std::string ip_;
     in_port_t port_;
-
+    int fd_;
+    bool handshakeDone_ = false;
   };
 
 } // namespace Network
